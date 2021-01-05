7f9bb12ac1419428966583a215ff87d93a8ca9b7
mi7ai <hottmayer@gmail.com>
2021-01-05 01:03:52 +0100

    arrays

---
+++ public/5_arrays.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body bgcolor="gray">
+    <h1>TASK: ARRAYS & INDEXES</h1>
+    <?php
+    $a = ["Mihai", "Mihai2", "Mihai3"];
+
+    echo "<p>Names:";
+    echo var_dump($a);
+    echo "<p>";
+
+    echo "<pre>:";
+    echo var_dump($a);
+    echo "<pre>";
+    ?>
+
+    <h1>TASK: ARRAYS & INDEXES</h1>
+</body>
+</html>
+
+
